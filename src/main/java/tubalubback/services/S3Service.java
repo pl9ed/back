5a78551c0ea60597376a583d2bb89ebe988ea815
Pai Li <pl9ed15@gmail.com>
2021-02-25 01:19:02 -0500

    Added logging for presignPutUrl

--- src/main/java/tubalubback/services/S3Service.java
+++ src/main/java/tubalubback/services/S3Service.java
@@ -41,6 +41,7 @@ public class S3Service {
     }
 
     public String presignPutUrl(String filename) {
+        log.info("{} : Generated presigned requestion for: {}", System.getenv("AWS_ACCESS_KEY_ID"), filename);
         PresignedPutObjectRequest putReq = presigner.presignPutObject(r -> r.signatureDuration(Duration.ofMinutes(2))
                 .putObjectRequest(por -> por.bucket(BUCKET_NAME).key(BUCKET_PATH+filename)));
         //log.info("Presigned URL generated: {}", putReq.url().toString());
