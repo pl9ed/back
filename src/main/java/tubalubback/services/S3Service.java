60f304534d4de394c0de3add07d95155ac3ab1c1
Pai Li <pl9ed15@gmail.com>
2021-02-25 00:30:13 -0500

    Revert "Added public read ACL to put object request"
    This reverts commit 9356af7937377b8cccfad017a87f2141b9d74965.

--- src/main/java/tubalubback/services/S3Service.java
+++ src/main/java/tubalubback/services/S3Service.java
@@ -42,8 +42,7 @@ public class S3Service {
 
     public String presignPutUrl(String filename) {
         PresignedPutObjectRequest putReq = presigner.presignPutObject(r -> r.signatureDuration(Duration.ofMinutes(2))
-                .putObjectRequest(por ->
-                        por.bucket(BUCKET_NAME).key(BUCKET_PATH+filename).acl(ObjectCannedACL.PUBLIC_READ)));
+                .putObjectRequest(por -> por.bucket(BUCKET_NAME).key(BUCKET_PATH+filename)));
         //log.info("Presigned URL generated: {}", putReq.url().toString());
         return putReq.url().toString();
     }
